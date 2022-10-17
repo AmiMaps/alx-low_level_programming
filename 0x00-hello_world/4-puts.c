f830988dcf26a0567accdc947fac1e3d961c10ee
AmiMaps <mapulanaamanda@gmail.com>
2022-10-17 14:23:39 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	push("\"Programming is like buildinga multi linqual puzzel");
+
+	    return (0);
+
+}
