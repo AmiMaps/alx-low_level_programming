efaf223860307e37b39cb2438eab4447b180808c
AmiMaps <mapulanaamanda@gmail.com>
2022-10-20 08:41:24 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - returns alphbets 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	while (ch < = 'z')
+
+{
+	putchar (ch);
+	ch++;
+
+}
+        putchar ('\n');
+	return (0);
+
+}
